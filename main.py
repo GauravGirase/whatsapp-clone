0ede3d540aa4a17064b2d88553ff0c611c1ec60e
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2026-03-09 15:51:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOW_ORIGINS if settings.DEBUG else ["https://yourdomain.com"],
+    allow_origins=["*"] if settings.DEBUG else ["https://yourdomain.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
